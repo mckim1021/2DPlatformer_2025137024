72f586434dde1b515affaf81167ee150ddc36082
mckim1021 <kimminchan.me@gmail.com>
2025-04-17 04:58:17 +0900

    캐릭터 애니메이션 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
+    public float moveSpeed = 1f;
+    public float jumpForce = 1.5f;
     public Transform GroundCheck;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator pAni;
     private bool isGrounded;
 
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
     }
     private void Update()
     {
@@ -26,7 +29,23 @@ public class PlayerController : MonoBehaviour
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
         }
 
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        if (moveInput < 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        if (isGrounded && Input.GetKey(KeyCode.A))
+        {
+            pAni.SetTrigger("RunAction");
+        }
+
+        if (isGrounded && Input.GetKey(KeyCode.D))
+        {
+            pAni.SetTrigger("RunAction");
+        }
     }
 }
